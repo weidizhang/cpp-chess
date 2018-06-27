6fc7f6d1b86479c3bd8aab6d80c3d3e7e445a22c
Weidi Zhang <weidiz999@yahoo.com>
2018-06-27 13:30:15 -0700

    Add different piece types

--- headers/piece.h
+++ headers/piece.h
@@ -4,13 +4,22 @@
 class Piece
 {
     char type;
+    char color;
     int val;
 public:
     Piece();
-    Piece(char newType, int newVal);
+    Piece(char newType, char newColor, int newVal);
 
     char getType();
+    char getColor();
     int getValue();
 };
+
+struct King : public Piece { King(char newColor); };
+struct Queen : public Piece { Queen(char newColor); };
+struct Rook : public Piece { Rook(char newColor); };
+struct Bishop : public Piece { Bishop(char newColor); };
+struct Knight : public Piece { Knight(char newColor); };
+struct Pawn : public Piece { Pawn(char newColor); };
 
 #endif
--- piece.cpp
+++ piece.cpp
@@ -1,11 +1,11 @@
 #include "headers/piece.h"
 
 Piece::Piece()
-{    
+{
 }
 
-Piece::Piece(char newType, int newVal)
-    : type(newType), val(newVal)
+Piece::Piece(char newType, char newColor, int newVal)
+    : type(newType), color(newColor), val(newVal)
 {
 }
 
@@ -14,7 +14,19 @@ char Piece::getType()
     return type;
 }
 
+char Piece::getColor()
+{
+    return color;
+}
+
 int Piece::getValue()
 {
     return val;
 }
+
+King::King(char newColor) : Piece('K', newColor, 10000) {}
+Queen::Queen(char newColor) : Piece('Q', newColor, 1000) {}
+Rook::Rook(char newColor) : Piece('R', newColor, 525) {}
+Bishop::Bishop(char newColor) : Piece('B', newColor, 350) {}
+Knight::Knight(char newColor) : Piece('N', newColor, 350) {}
+Pawn::Pawn(char newColor) : Piece('P', newColor, 100) {}
